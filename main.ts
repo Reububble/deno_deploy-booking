cfa2a1b41ac6375acde82fbcc9c5d2f836319218
Reububble <Reububble@gmail.com>
2024-05-25 18:04:32 +1000

    dist

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 Deno.serve(async (request) => {
   const url = new URL(request.url);
 
-  using file = await Deno.open(import.meta.resolve("." + url.pathname)).catch(() => undefined) ??
-    await Deno.open(import.meta.resolve("." + url.pathname + "/index.html")).catch(() => undefined);
+  using file = await Deno.open(import.meta.resolve("./dist/" + url.pathname)).catch(() => undefined) ??
+    await Deno.open(import.meta.resolve("./dist/" + url.pathname + "/index.html")).catch(() => undefined);
 
   if (file === undefined) {
     return new Response(undefined, { status: 404 });
