1a22e5c07ac56173c636d382f4c2bd706f2df0de
Reububble <Reububble@gmail.com>
2024-05-25 18:02:38 +1000

    without redirect

--- main.ts
+++ main.ts
@@ -1,13 +1,6 @@
 Deno.serve(async (request) => {
   const url = new URL(request.url);
 
-  if (url.pathname.endsWith("/")) {
-    return new Response(undefined, {
-      "status": 308,
-      "headers": { "Location": url.pathname.substring(0, url.pathname.length - 1) },
-    });
-  }
-
   using file = await Deno.open(import.meta.resolve("." + url.pathname)).catch(() => undefined) ??
     await Deno.open(import.meta.resolve("." + url.pathname + "/index.html")).catch(() => undefined);
 
